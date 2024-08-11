ac3cad66ec202c3dd4e80668b363e43cc307e46f
Oltcit <edgardo.banga@gmail.com>
2024-08-11 11:35:34 -0300

    Segunda subida alumno1

--- Suma/src/Suma.java
+++ Suma/src/Suma.java
@@ -7,9 +7,9 @@ public class Suma {
 		Scanner sc= new Scanner(System.in);
 		int a, b;
 		int res;
-		System.out.println("Ingrese el primer número: ");
+		System.out.println("Alumno, Ingrese el primer número: ");
 		a=sc.nextInt();
-		System.out.println("Ingrese el segundo número: ");
+		System.out.println("Alumno, Ingrese el segundo número: ");
 		b=sc.nextInt();
 		res=a+b;
 		System.out.println("La suma es: "+res);
