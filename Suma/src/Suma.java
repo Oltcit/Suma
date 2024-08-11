9c3fa43247f5c7dcc76c7d1c0a64e1a5763e598e
Oltcit <edgardo.banga@gmail.com>
2024-08-11 11:26:17 -0300

    Primer Subida alumno1

--- Suma/src/Suma.java
+++ Suma/src/Suma.java
@@ -6,11 +6,13 @@ public class Suma {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
 		int a, b;
+		int res;
 		System.out.println("Ingrese el primer número: ");
 		a=sc.nextInt();
 		System.out.println("Ingrese el segundo número: ");
 		b=sc.nextInt();
-		System.out.println("La suma es: "+(a+b));
+		res=a+b;
+		System.out.println("La suma es: "+res);
 	}
 
 }
