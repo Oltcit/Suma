3aae68fca3f68fa18d15d06d5230706f0d5df0b6
Oltcit <edgardo.banga@gmail.com>
2024-08-11 10:04:44 -0300

    Primer subida

---
+++ Suma/src/Suma.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int a, b;
+		System.out.println("Ingrese número");
+		a=sc.nextInt();
+		System.out.println("Ingrese número");
+		b=sc.nextInt();
+		System.out.println("La suma es: "+(a+b));
+	}
+
+}
